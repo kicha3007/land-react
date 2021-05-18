c2bb312647aced471996490e5410451eb347d53e
kicha3007 <kicha3007@gmail.com>
2021-05-18 22:36:39 +0300

    настроил babel

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App =  () => {
+    console.log("yabadabadooo!!");
+    return <div>hi</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
