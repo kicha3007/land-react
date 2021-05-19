d862359a66a98fed04439d836c9827a3d04d9fbf
Sergey Kichigin <kichigin@aicrobotics.ru>
2021-05-19 15:20:17 +0300

    Добавил основные настройки вебпака

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+import Log from "./log"
+import Calc from "./calc"
+import img from "./react-icon.png";
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
+import "./main.scss";
 
 const App =  () => {
-    console.log("yabadabadooo!!");
-    return <div>hi</div>;
+  console.log("yabadabadooo!!");
+  return <div>Hello</div>;
 }
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+
+const el = document.createElement("img");
+el.src = img;
+document.body.appendChild(el);
+
+const calc = new Calc();
+const log = new Log();
+
+
+log.log(calc.add(1,2,3))
+
